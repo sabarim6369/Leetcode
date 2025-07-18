17e7d51c0d1386d339d22ada28231796798f6187
Sabari M <151687270+sabarim6369@users.noreply.github.com>
2025-07-18 23:31:33 +0530

    Time: 79 ms (48.51%) | Memory: 146.1 MB (96.14%) - LeetSync

--- 523-continuous-subarray-sum/continuous-subarray-sum.cpp
+++ 523-continuous-subarray-sum/continuous-subarray-sum.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     bool checkSubarraySum(vector<int>& nums, int k) {
-      int n=nums.size();
-       int prefixsum=nums[0];
-       unordered_map<int,int>mp;
-       int count=0;
-       mp[nums[0]%k]=0;
-      for(int i=1;i<n;i++){
-        prefixsum+=nums[i];
-        int mod=prefixsum%k;
-        if(prefixsum%k==0)return true;
-        if(mp.find(mod)!=mp.end()){
-            if(i-mp[mod]>=2){
+        int n = nums.size();
+        int prefixsum = nums[0];
+        unordered_map<int, int> mp;
+        int count = 0;
+        mp[nums[0] % k] = 0;
+        for (int i = 1; i < n; i++) {
+            prefixsum += nums[i];
+            int mod = prefixsum % k;
+            if (prefixsum % k == 0)
                 return true;
+            if (mp.find(mod) != mp.end()) {
+                if (i - mp[mod] >= 2) {
+                    return true;
+                }
+            } else {
+                mp[mod] = i;    //as we need greater than or equal to dont try to shrink,Let try to expand
             }
         }
-        else{        mp[mod]=i;
-}
-
-      }
-      return false;
+        return false;
     }
 };
