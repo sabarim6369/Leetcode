7d9dbc2deeb0a8af29b5844ef906609cb5309ed2
Sabari M <151687270+sabarim6369@users.noreply.github.com>
2025-07-21 17:34:15 +0530

    Time: 16 ms (32.91%) | Memory: 11.2 MB (21.87%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -7,15 +7,21 @@ public:
         int left=0;
         for(int right=0;right<n;right++){
            mp[s[right]]++;
-           while(mp.size()==3){
+        //    while(mp.size()==3){
+        //     count++;
+        //     count+=(n-(right+1));
+        //     mp[s[left]]--;
+        //     if(mp[s[left]]==0){
+        //         mp.erase(s[left]);
+        //     }
+        //     left++;
+        //    }
+        while(mp['a']>0 && mp['b']>0 && mp['c']>0){
             count++;
             count+=(n-(right+1));
             mp[s[left]]--;
-            if(mp[s[left]]==0){
-                mp.erase(s[left]);
-            }
             left++;
-           }
+        }
 
 
         }
