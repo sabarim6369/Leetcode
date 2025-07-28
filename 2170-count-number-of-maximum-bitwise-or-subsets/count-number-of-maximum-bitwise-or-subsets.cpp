ba42c515b50ec69fbbd26093e7366b0a8ecc7840
Sabari M <151687270+sabarim6369@users.noreply.github.com>
2025-07-28 20:53:06 +0530

    Time: 79 ms (25.12%) | Memory: 10.6 MB (86.47%) - LeetSync

--- 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.cpp
@@ -9,27 +9,20 @@ public:
             int val = i;
             vector<int> inner;
             int k = 0;
+            int tempOr = 0;
             while (val) {
                 if (val & 1) {
-                    inner.push_back(nums[k]);
+                     tempOr |= nums[k];
                 }
                 k++;
                 val >>= 1;
             }
-            if (inner.size() == 0) {
-                continue;
-            } else {
-                int temp = inner[0];
-                for (int j = 1; j < inner.size(); j++) {
-                    temp |= inner[j];
-                }
-
-                if (temp > maxOr) {
-                    maxOr = temp;
-                    count = 1;
-                } else if (temp == maxOr) {
-                    count++;
-                }
+           
+            if (tempOr > maxOr) {
+                maxOr = tempOr;
+                count = 1;
+            } else if (tempOr == maxOr) {
+                count++;
             }
         }
         return count;
