fa096c859d3c4face2acf02f48f1b4158fabc242
Sabari M <151687270+sabarim6369@users.noreply.github.com>
2025-08-02 22:07:53 +0530

    Time: 0 ms (100.00%) | Memory: 22.9 MB (83.80%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -11,26 +11,41 @@
  */
 class Solution {
 public:
-bool isbalanced=true;
-     int maxdepth(TreeNode *node){
-        if(!node)return 0;
-        return 1+max(maxdepth(node->left),maxdepth(node->right));
-     }
-     void preorder(TreeNode *node){
-        if(!node || !isbalanced)return;
-        int left=maxdepth(node->left);
-        int right=maxdepth(node->right);
-        if(abs(left-right)>1){
-            isbalanced=false;
-            return;
-        }
-        preorder(node->left);
-        preorder(node->right);
-     }
-    bool isBalanced(TreeNode* root) {
-        preorder(root);
-        return isbalanced;
+// bool isbalanced=true;
+//      int maxdepth(TreeNode *node){
+//         if(!node)return 0;
+//         return 1+max(maxdepth(node->left),maxdepth(node->right));
+//      }
+//      void postorder(TreeNode *node){
+//         if(!node || !isbalanced)return;
+//         int left=maxdepth(node->left);
+//         int right=maxdepth(node->right);
+//         if(abs(left-right)>1){
+//             isbalanced=false;
+//             return;
+//         }
+//         preorder(node->left);
+//         preorder(node->right);
+//      }
+//     bool isBalanced(TreeNode* root) {
+//         postorder(root);
+//         return isbalanced;
        
+        
+//     }
+// };
+int dfs(TreeNode *node){
+    if(!node)return 0;
+    int left=dfs(node->left);
+    if(left==-1)return -1;
+    int right=dfs(node->right);
+    if(right==-1)return -1;
+    if(abs(left-right)>1)return -1;
+    return 1+max(left,right);
+
+}
+ bool isBalanced(TreeNode* root) {
+       return dfs(root)!=-1;
         
     }
 };
